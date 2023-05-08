523cd6a3fc2f192cc4ba8c719ba72effe239cbb9
bareum-dev <bareum.dev@gmail.com>
2023-05-08 13:42:51 +0300

    feat: done 02-write-file (without exit)

---
+++ 02-write-file/index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+const { stdin, stdout } = process;
+
+fs.writeFile(path.join(__dirname, "text.txt"), "", (error) => {
+  if (error) throw error;
+});
+
+stdout.write(
+  "Hello.\ntext.txt file was created.\nWrite something to add to the file\nWrite 'stop' to end writing to file\n"
+);
+
+stdin.on("data", (message) => {
+  if (message == "exit") {
+    console.log("Writing to file has been completed");
+    process.exit();
+  } else
+    fs.appendFile(path.join(__dirname, "text.txt"), message, (error) => {
+      if (error) throw error;
+    });
+});
